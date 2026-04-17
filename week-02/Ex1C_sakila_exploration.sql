4501154fef4d96519da489c5a5234e29e2f7d900
Chantal.Lee <leechantal83@gmail.com>
2026-04-16 21:41:04 -0400

    updated Ex1C

---
+++ week-02/Ex1C_sakila_exploration.sql
@@ -0,0 +1,11 @@
+/*
+a) actor id, first name, last name, and last update
+b) info for films you can rent such as description, title, rating, and rental rate
+c) film_actor
+d) the date rental was rented and returned, the id of the customer who did it, and the rental id number
+e) the id's of the store, film, inventory, and the last update
+f) In the rental and film tables you can find names of films and rented date. 
+*/
+
+SELECT * FROM rental;
+SELECT * FROM film;
