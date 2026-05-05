37ff3c50bd68d9c973d7f46581a89b3faff4b412
Chantal.Lee <leechantal83@gmail.com>
2026-05-05 19:03:17 -0400

    git commit -m "updated again"

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,19 @@
 print('Hello world!')
+
+message = "Hello world!"
+print(message)
+
+#Displaying dollars and cents
+
+dollars = 3
+cents = .50
+
+print(dollars + cents)
+
+cents = .25
+print(dollars + cents)
+
+d_str = "3 dollars "
+c_str = "50 cents"
+
+print(d_str + c_str)
