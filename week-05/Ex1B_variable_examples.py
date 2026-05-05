d11b2ecf5156323d9341f515e6c9ad4b05b97957
Chantal.Lee <leechantal83@gmail.com>
2026-05-05 19:41:41 -0400

    git commit -m "added Ex1B"

---
+++ week-05/Ex1B_variable_examples.py
@@ -0,0 +1,28 @@
+c_id = 234
+customer_name = "Chantelle Healthy"
+customer_gender = "Female"
+dob = "2/1/2024"
+drivers_license = (f"Dontlook{84}")
+auto_p_n = 12345
+
+print(customer_name + ", " + customer_gender + ", " + dob + ", " + drivers_license)
+
+my_name = "Chantal"
+City = "Sacramento"
+State = "Cali"
+
+
+# a) Words you cannot use are:
+# False,def,if,raise,assert,finally,nonlocal,yield
+# None,del,import,return,class,form,or,
+# True,elif,in,try,break,for,not,
+# and,else,is,while,continue,global,pass,
+# as,except,lambda,with,
+
+# b) 5 words and their uses:
+# finally = always executes the code regardless of whether 
+    # an error was thrown or not
+# yield = ends a function and returns an iterator
+# as =  creates an alias
+# is = check if the two variables are equal
+# break = used to come out of a loop
