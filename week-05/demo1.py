c96d541b459f7e4afe360ae2f75d8b62f5c48a25
Chantal.Lee <leechantal83@gmail.com>
2026-05-05 19:00:53 -0400

    git commit -m "updated"

---
+++ week-05/demo1.py
@@ -0,0 +1,30 @@
+#Chantal Lee
+# 5/5/26
+
+name = "Hello"
+print (f"Original string : {name}")
+print(f"First character : {name[0]}")
+
+
+Classmate_1 = "Chantal Lee"
+Classmate_2 = "Dimitri Nji"
+Classmate_3 = "Vesna Cari"
+
+print(f"\n{Classmate_1}, \n{Classmate_2}, \n{Classmate_3}")
+
+
+
+name = input ("Enter your name: ")
+print(f"Hello {name}")
+
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+print(f"You have entered {num1} and {num2}")
+
+print(type(num1))
+print(type(num2))
+
+print(f"The sum of {num1} and {num2} is {num1+num2}")
+print(f"The product of {num1} and {num2} is {num1*num2}")
+print(f"The product of 2 and {num2} is {2*num2}")
