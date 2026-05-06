ef51e74a2459dc636ae218dd00d9168e72cd6189
Chantal.Lee <leechantal83@gmail.com>
2026-05-06 16:44:26 -0400

    git commit -m "updated excer"

---
+++ week-05/Exercises/5/6/26class.py
@@ -0,0 +1,41 @@
+C = input("Enter first number ")
+D = input("Enter second number ")
+E = input("Enter third number ")
+
+#average = ({C} + {D} + {E}/3)
+#print(average)
+
+#print(type())
+
+
+
+price = input("Enter original price $")
+discount = input ("Enter discount percentage $")
+
+#print(f"${price (discount: .2f)}")
+
+meal = input("Enter meal cost ")
+#tip = 0.20
+tax = 0.0025
+
+#tipamount = meal * tip
+#taxamount = meal * tax
+#totalcost = meal + tipamount + taxamount
+
+print("Meal Cost Breakdown")
+#print(f"Meal Cost: ${meal;2f}")
+
+
+
+
+
+classmates = ("Chantal", "Dimitri", "Patrick", "Oluwafemi", "Myleah")
+
+print(classmates[0])
+print(classmates[1])
+print(classmates[2])
+
+
+name = {"Julia" :"NewYork", "Marty":"Ohio", "Juan":"Maine"}
+
+print(f"Julia"{name})
