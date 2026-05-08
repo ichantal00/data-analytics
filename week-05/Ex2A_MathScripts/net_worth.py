74b045f2a29869e35ae43be30546a0f35d748146
Chantal.Lee <leechantal83@gmail.com>
2026-05-08 18:54:15 -0400

    git commit -m "updated net_worth"

--- week-05/Ex2A_MathScripts/net_worth.py
+++ week-05/Ex2A_MathScripts/net_worth.py
@@ -7,6 +7,18 @@ print(f"Your total debts are ${debts}")
 print(f"Your net worth is ${n_w}")
 
 
+#Birthday area of a rectangle
 
+birthday = int(input("What day is your birthday? "))
+month = int(input("What month(number) does your birthday fall in? "))
+area = birthday * month
 
+print(area)
 
+SideA = birthday
+SideB = month
+
+print(f"Side A is {SideA}")
+print(f"Side B is {SideB}")
+
+print(f"The area of the rectangle is {area}")
