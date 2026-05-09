b719178ff15c3b769c444034472dc1475ca6e100
Chantal.Lee <leechantal83@gmail.com>
2026-05-09 11:22:32 -0400

    git commit -m "finished 3A string"

--- week-05/Ex3A_string_cleaning.py
+++ week-05/Ex3A_string_cleaning.py
@@ -9,7 +9,7 @@ salary_2 = "$74,000"
 name_1 = name_1.lower()
 name_2 = name_2.lower()
 name_3 = name_3.lower()
-print(name_1 and name_2 and name_3)
+print(name_1, name_2, name_3)
 
 # 1st letters capitalized
 name_1 = "PRIYA SHARMA"
@@ -18,12 +18,20 @@ name_3 = "LaTonya Williams"
 salary_1 = "$82,500"
 salary_2 = "$74,000"
 
-name_1 = name_1.title(0, 6)
+name_1 = name_1.title()
 name_2 = name_2.title()
 name_3 = name_3.title()
-print(name_1 and name_2 and name_3)
+print(name_1, name_2, name_3)
 
 #replacing dollar signs with spaces
 
 salary_1 = salary_1.replace("$", " ")
 salary_2 = salary_2.replace("$", " ")
+
+print(salary_1, salary_2)
+
+salary_1 = int(salary_1.replace(",", ""))
+salary_2 = int(salary_2.replace(",", ""))
+
+print(type(salary_1))
+print(type(salary_2))
