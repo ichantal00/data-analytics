a5488d6cfcab1d572ce9a5277050f1283bdc279a
Chantal.Lee <leechantal83@gmail.com>
2026-05-09 12:14:56 -0400

    git commit -m "added movielist"

---
+++ week-05/Ex3B_SampleCollections/movie_list.py
@@ -0,0 +1,35 @@
+#First Attempt --  My top favorite movies --
+
+Mov1 = "Train to Busan"
+Mov2 = "I am Legend"
+Mov3 = "Brave"
+
+print(f"The list {Mov1} includes my top {len(Mov1)} favorite movies")
+
+# My Top Favorite Movies --  --
+MovList = ["Train to Busan", "I am Legend", "Brave"]
+
+print(f"The list {MovList} includes my top {len(MovList)} favorite movies")
+
+
+Snacks = ["Chili Seaweed", "Mango Pops" "Almond Choco"]
+
+Fruit = ["Mango", "Blueberry", "Kiwi", "Grapes"]
+
+print("----- Sorted Fruit")
+print(sorted(Fruit))
+
+print("----- Un-sorted Sorted Fruit")
+print(sorted(Fruit))
+
+
+print("----- Sorted Snacks")
+Snack = Snacks.sort()
+print(Snack) #Printed "None"
+
+MovList.append("Man From Nowhere")
+
+print(MovList)
+
+print(f"The list MovList includes my top {len(MovList)} favorite movies")
+print(f"This list includes{MovList}")
